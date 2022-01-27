23a3cbf3ca7acb1ff72fcab6ea38595a42b1f18d
Dario Castiglione <dario.castiglione99@gmail.com>
2022-01-27 19:27:27 +0100

    add modale

--- js/app.js
+++ js/app.js
@@ -6,53 +6,68 @@ const word = document.querySelector(".word p");
 const inputText = document.querySelector("#txt");
 const timeText = document.querySelector(".time")
 const scoreText = document.querySelector(".score");
+const modale = document.querySelector(".modale");
+const wrapper = document.querySelector(".game");
+const modaleP = document.querySelector(".modale p");
+const modaleBtn = document.querySelector(".modale button");
+
+modaleBtn.addEventListener("click", () => {
+    modale.style.display="none";
+    wrapper.style.opacity="1"
+})
 
 let currentWord = "";
-    let time = 15;
-    let score = 0;
-
-
-function gameStop(){
-    alert(`Hai fatto ${score} punti`);
-    score = 0;
-    time = 15;
-    timeText.innerText= `00:${time}`;
-    scoreText.innerText = score;
-
-    
+let time = 15;
+let score = 0;
+function spawnModale(modale){
+    modale.style.display="block";
+    modaleP.innerText=`hai fatto ${score} punti`
+    wrapper.style.opacity="0.3"
 }
 
-function startCounter(){
-    timeText.innerText= `00:${time}`
-    const timepass =  setInterval(() => {
+function gameStop() {
+    spawnModale(modale)
+    score = 0;
+    time = 15;
+    timeText.innerText = `00:${time}`;
+    scoreText.innerText = score;
+    inputText.value = ""
+}
+function startCounter() {
+    timeText.innerText = `00:${time}`
+    const timepass = setInterval(() => {
         time--
-        timeText.innerText= `00:${time}`
+        timeText.innerText = `00:${time}`
         if (time === 0) {
             clearInterval(timepass)
             gameStop();
-        } 
+        }
     }, 1000);
 }
-function nextLevel(){
+function nextLevel() {
     time += 2;
     inputText.value = "";
     score++;
     scoreText.innerText = score;
-    start();
+    generateWord();
 }
-function handleInput(){
-    inputText.addEventListener("keyup", (e) => {
-        e.target.value === currentWord ? nextLevel() 
-        : "";  
-    })
-}
-function start(){
+function generateWord() {
     currentWord = words[Math.floor(Math.random() * words.length)]
     word.innerText = currentWord;
 }
 
-startBtn.addEventListener("click", () => {
+
+//------------------------------START-------------------------------//
+function start() {
     startCounter();
-    handleInput();
-    start()
+    generateWord();
+
+    inputText.addEventListener("keyup", (e) => {
+        e.target.value.toLowerCase() === currentWord && nextLevel();
+            
+    })
+}
+
+startBtn.addEventListener("click", () => {
+    start();
 })
